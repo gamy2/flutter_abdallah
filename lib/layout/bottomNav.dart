0d0f502699158ce96819aca935d5ec4e3518875e
mohamed gamal <76060169+gamy2@users.noreply.github.com>
2022-07-19 15:11:20 +0200

    error in db

--- lib/layout/bottomNav.dart
+++ lib/layout/bottomNav.dart
@@ -2,6 +2,7 @@ import 'package:f_ab_study/modules/bottom_nav/arch/arch.dart';
 import 'package:f_ab_study/modules/bottom_nav/done/doneTask.dart';
 import 'package:f_ab_study/modules/bottom_nav/task/tasks.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomeNav extends StatefulWidget {
   HomeNav({Key? key}) : super(key: key);
@@ -11,33 +12,43 @@ class HomeNav extends StatefulWidget {
 }
 
 class _HomeNavState extends State<HomeNav> {
-  int _currentIndex=1;
-  List<Widget> screens=[
-    ArchPage(),
-    Tasks(),
-    DoneTask()
-  ];
-  List<String> titles=[
-    "Arch","Tasks","Done"
-  ];
+  int _currentIndex = 1;
+  List<Widget> screens = [ArchPage(), Tasks(), DoneTask()];
+  List<String> titles = ["Arch", "Tasks", "Done"];
+  @override
+  void initState() {
+    super.initState();
+    createDataBase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(titles[_currentIndex]),
       ),
-      bottomNavigationBar: BottomNavigationBar(currentIndex: _currentIndex,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.account_balance), label: "account"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance), label: "account"),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.edit), label: "Edit")
-        ],onTap: (index){
-setState(() {
-  _currentIndex=index;
-});
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
         },
       ),
-      body:screens[_currentIndex] ,
+      body: screens[_currentIndex],
     );
   }
+}
+
+void createDataBase() async {
+  // var databasesPath = await getDatabasesPath();
+  var db = openDatabase("tasks.db", version: 1, onCreate: (db, ver) {
+    print("object");
+  });
 }
